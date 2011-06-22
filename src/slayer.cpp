a02e2f55097c67582584b60b210e34d6e0a8c723
meleneth <meleneth@marvin.sectorfour>
2011-06-21 18:12:14 -0700

    Lua embed works now but Im not happy about the build system hackery

--- src/slayer.cpp
+++ src/slayer.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 
 /* Include the Lua API header files. */
-#include <lua.h>
+#include <lua.hpp>
 #include <lauxlib.h>
 #include <lualib.h>
 
@@ -14,6 +14,7 @@ using namespace std;
 Console *console;
 
 void do_lua_stuff();
+static void openlualibs(lua_State *l);
 
 int main(int argc, char *argv[])
 {
@@ -29,11 +30,32 @@ int main(int argc, char *argv[])
   return 0;
 }
 
+static const luaL_reg lualibs[] =
+{
+  { "base",       luaopen_base },
+  { NULL,         NULL }
+};
+
+
+/* A function to open up all the Lua libraries you declared above. */
+static void openlualibs(lua_State *l)
+{
+    const luaL_reg *lib;
+
+    for (lib = lualibs; lib->func != NULL; lib++)
+    {
+        lib->func(l);
+        lua_settop(l, 0);
+    }
+}
+
+
 void do_lua_stuff()
 {
   /* Declare a Lua State, open the Lua State and load the libraries (see above). */
   lua_State *l;
   l = lua_open();
+  openlualibs(l);
   
   /* You can do what you want here. Note: Remember to update the libraries used (see above) */
   /* if you add to your program and use new Lua libraries. */
@@ -42,7 +64,7 @@ void do_lua_stuff()
   /* Plus print some text directly from C. */
   printf("This line in directly from C\n\n");
 //  lua_dofile(l, "script.lua");
-  lua_dostring(l, "print('woozy')");
+  luaL_dostring(l, "print('woozy')");
   printf("\nBack to C again\n\n");
   
   /* Remember to destroy the Lua State */
