b0281c3e9af77ac6cf94360e98523fc2989e3d83
meleneth <meleneth@marvin.sectorfour>
2011-06-21 17:51:12 -0700

    Try some LUA

--- src/slayer.cpp
+++ src/slayer.cpp
@@ -1,5 +1,11 @@
 #include<iostream>
 
+/* Include the Lua API header files. */
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
+
+
 #include<entitymgr.hpp>
 #include<console.hpp>
 
@@ -7,11 +13,39 @@ using namespace std;
 
 Console *console;
 
+void do_lua_stuff();
+
 int main(int argc, char *argv[])
 {
   console = new Console();
   EntityMgr *stuff = new EntityMgr();
+
+  do_lua_stuff();
+
   cout << "Slayer 1.0"  << endl;
-  
+   
+  delete stuff;
+  delete console;
   return 0;
+}
+
+void do_lua_stuff()
+{
+  /* Declare a Lua State, open the Lua State and load the libraries (see above). */
+  lua_State *l;
+  l = lua_open();
+  
+  /* You can do what you want here. Note: Remember to update the libraries used (see above) */
+  /* if you add to your program and use new Lua libraries. */
+  /* In the lines below, I load and run the Lua code contained in the file */
+  /* "script.lua". */
+  /* Plus print some text directly from C. */
+  printf("This line in directly from C\n\n");
+//  lua_dofile(l, "script.lua");
+  lua_dostring(l, "print('woozy')");
+  printf("\nBack to C again\n\n");
+  
+  /* Remember to destroy the Lua State */
+  lua_close(l);
+  
 }
