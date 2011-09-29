6446e63f55da852e5bc3228c4bb909ccd1df232d
meleneth <meleneth@marvin.sectorfour>
2011-09-29 00:12:00 -0700

    Cleanup executable and start arranging for a persistant lua interpreter

--- src/slayer.cpp
+++ src/slayer.cpp
@@ -8,66 +8,39 @@
 
 #include<entitymgr.hpp>
 #include<console.hpp>
+#include<video.hpp>
 
 using namespace std;
 
 Console *console;
 
-void do_lua_stuff();
-static void openlualibs(lua_State *l);
+lua_State *slayer_lua_state;
+
+extern "C" {
+  int luaopen_Slayer(lua_State* L);
+};
 
 int main(int argc, char *argv[])
 {
   console = new Console();
+  console->print_logs = true;
+
+  slayer_lua_state = lua_open();
+  luaopen_base(slayer_lua_state);
+  luaopen_Slayer(slayer_lua_state);
+ 
+  Video *video = new Video(1280, 1024);
   EntityMgr *stuff = new EntityMgr();
 
-  do_lua_stuff();
+  console->log("Slayer 1.0");
 
-  cout << "Slayer 1.0"  << endl;
-   
   delete stuff;
+  delete video;
+
+  lua_close(slayer_lua_state);
+
   delete console;
+
   return 0;
 }
 
-static const luaL_reg lualibs[] =
-{
-  { "base",       luaopen_base },
-  { NULL,         NULL }
-};
-
-
-/* A function to open up all the Lua libraries you declared above. */
-static void openlualibs(lua_State *l)
-{
-    const luaL_reg *lib;
-
-    for (lib = lualibs; lib->func != NULL; lib++)
-    {
-        lib->func(l);
-        lua_settop(l, 0);
-    }
-}
-
-
-void do_lua_stuff()
-{
-  /* Declare a Lua State, open the Lua State and load the libraries (see above). */
-  lua_State *l;
-  l = lua_open();
-  openlualibs(l);
-  
-  /* You can do what you want here. Note: Remember to update the libraries used (see above) */
-  /* if you add to your program and use new Lua libraries. */
-  /* In the lines below, I load and run the Lua code contained in the file */
-  /* "script.lua". */
-  /* Plus print some text directly from C. */
-  printf("This line in directly from C\n\n");
-//  lua_dofile(l, "script.lua");
-  luaL_dostring(l, "print('woozy')");
-  printf("\nBack to C again\n\n");
-  
-  /* Remember to destroy the Lua State */
-  lua_close(l);
-  
-}
