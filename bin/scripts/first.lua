2e6daf256f248fccbe3cc4b268f5b132eb78a17a
meleneth <meleneth@marvin.sectorfour>
2011-10-17 14:45:15 -0700

    Add another bad entity.  Entity code looking really old and crusty to my new eyes

--- bin/scripts/first.lua
+++ bin/scripts/first.lua
@@ -12,3 +12,11 @@ e.v.x = 256
 
 m:add_entity(e)
 
+e = Fuego.Entity();
+e.texture = Fuego.Texture(0, "/sprites/badship1.png")
+e.v.x = -300 
+e.v.rotation = 1.0
+e.rotation = 64.0
+
+m:add_entity(e)
+
